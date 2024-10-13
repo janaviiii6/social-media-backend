a32cabe7f6a998eee156e287c5283d6373b118f1
JanaviK <kharatjanavi16@gmail.com>
2024-10-13 13:30:49 +0530

    Added route to fetch all user details for admin dashboard

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//GET request for fetching user details
+app.get('/user-details',(req,res) => {
+    const sql = 'SELECT * FROM user_details';
+
+    db.query(sql,(err,results) => {
+        if(err) {
+            console.err('Error fetching user-details:', err.message);
+            return res.status(500).json({ error: 'Database Error' });
+        }
+        res.status(200).json(results);
+    });
+});
+
 //Start server
 const PORT = 5000;
 app.listen(PORT, () => {
